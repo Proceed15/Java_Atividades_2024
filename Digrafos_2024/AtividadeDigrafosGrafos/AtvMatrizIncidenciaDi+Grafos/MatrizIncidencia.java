957e9d1b1da7c940fd9f2e69660c1cd25670f897
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2024-10-16 16:07:44 -0300

    Update MatrizIncidencia.java

--- Digrafos_2024/AtividadeDigrafosGrafos/AtvMatrizIncidenciaDi+Grafos/MatrizIncidencia.java
+++ Digrafos_2024/AtividadeDigrafosGrafos/AtvMatrizIncidenciaDi+Grafos/MatrizIncidencia.java
@@ -1,17 +1,44 @@
-package Digrafos_2024.matrizincidencia;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atvmatrizincidenciadi.grafos;
+
+/**
+ *
+ * @author 0031432412016
+ */
 
 import javax.swing.JOptionPane;
 
-public class MatrizIncidencia {
+public class AtvMatrizIncidenciaDiGrafos {
     public static void main(String[] args) {
-        String[] options = {"Grafo", "Digrafo"};
-        int escolha = JOptionPane.showOptionDialog(null, "Escolha o tipo de grafo", "Escolha", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        //String[] options = {"Grafo", "Digrafo"};
+        int op;
 
-        if (escolha == 0) {
-            Grafo();
-        } else {
-            Digrafo();
-        }
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog(
+                    "Menu:\n"
+                            + "1 - Acessar o Menu de Grafos\n"
+                            + "2 - Acessar o Menu de Digrafos\n"
+                            + "0 - Sair\n"
+                            + "Informe a opção desejada:"));
+
+        switch (op) {
+                case 1:
+                    Grafo();
+                    break;
+                case 2:
+                    Digrafo();
+                    break;
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Programa Finalizado.");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida.");
+            }
+        } while (op != 0);
     }
 
     private static void Grafo() {
@@ -19,11 +46,48 @@ public class MatrizIncidencia {
         int numArestas = NumRegistrador("Digite o número de arestas do grafo");
 
         Grafo grafo = new Grafo(numVertices, numArestas);
-        grafo.informarArestas();
-        grafo.imprimirMatrizIncidencia();
-        grafo.determinarGrau();
-        grafo.VerticesVizinhos();
-        grafo.informarArestasParalelasELacos();
+        int op;
+
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog(
+                    "Menu:\n"
+                            + "1 - Informe a Aresta\n"
+                            + "2 - Imprima a Matriz de Incidencia\n"
+                            + "3 - Determinar Grau de Entrada e de Saida dos Vertices\n"
+                            + "4 - Determinar Vertices Vizinhos\n"
+                            + "5 - Informar Arestas Paralelas E Lacos Existentes\n"
+                            + "6 - Voltar\n"
+                            + "0 - Sair\n"
+                            + "Informe a opção desejada:"
+            ));
+
+
+            
+            switch (op) {
+                case 1:
+                    grafo.informarArestas();
+                    break;
+                case 2:
+                    grafo.imprimirMatrizIncidencia();
+                    break;
+                case 3:
+                    grafo.determinarGrau();
+                    break;
+                case 4:
+                    grafo.VerticesVizinhos();
+                    break;
+                case 5:
+                    grafo.informarArestasParalelasELacos();
+                    break;
+                case 6:
+                    break;
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Programa Finalizado.");
+                    System.exit(0);
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida.");
+            }
+        } while (op != 0);     
     }
 
     private static void Digrafo() {
@@ -31,11 +95,46 @@ public class MatrizIncidencia {
         int numArestas = NumRegistrador("Digite o número de arestas do dígrafo");
 
         Digrafo digrafo = new Digrafo(numVertices, numArestas);
-        digrafo.informarArestas();
-        digrafo.imprimirMatrizIncidencia();
-        digrafo.determinarGrauEntradaESaida();
-        digrafo.VerticesVizinhos();
-        digrafo.informarArestasParalelasELacos();
+        int op;
+
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog(
+                    "Menu:\n"
+                            + "1 - Informe a Aresta\n"
+                            + "2 - Imprima a Matriz de Incidencia\n"
+                            + "3 - Determinar Grau de Entrada e de Saida dos Vertices\n"
+                            + "4 - Determinar Vertices Vizinhos\n"
+                            + "5 - Informar Arestas Paralelas E Lacos Existentes\n"
+                            + "6 - Voltar\n"
+                            + "0 - Sair\n"
+                            + "Informe a opção desejada:"
+            ));
+
+            switch (op) {
+                case 1:
+                    digrafo.informarArestas();
+                    break;
+                case 2:
+                    digrafo.imprimirMatrizIncidencia();
+                    break;
+                case 3:
+                    digrafo.determinarGrauEntradaESaida();
+                    break;
+                case 4:
+                    digrafo.VerticesVizinhos();
+                    break;
+                case 5:
+                    digrafo.informarArestasParalelasELacos();
+                    break;
+                case 6:
+                    break;
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Programa Finalizado.");
+                    System.exit(0);
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida.");
+            }
+        } while (op != 0);     
     }
 
     private static int NumRegistrador(String mensagem) {
@@ -48,3 +147,4 @@ public class MatrizIncidencia {
         }
     }
 }
+
